f0bb32c9216c05f0cd9e6bc28b90ab199c2b9f4e
Owenn-boop <owennalt@gmail.com>
2024-08-19 22:02:00 -0500

    fixed casing of workout schema

--- server/models/Workout.js
+++ server/models/Workout.js
@@ -11,7 +11,7 @@ const WorkoutSchema = new mongoose.Schema(
         type: String,
         required: true,
     },
-    workoutname: {
+    workoutName: {
         type: String,
         required: true,
         unique: true,
